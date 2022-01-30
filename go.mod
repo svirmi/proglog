bf5b4c6d51e4599cb3a319846c13982304168ad1
M.S <mikhail.svirkin@mail.ru>
2022-01-30 12:44:29 +0200

    index

--- go.mod
+++ go.mod
@@ -2,10 +2,16 @@ module proglog
 
 go 1.17
 
-require github.com/gorilla/mux v1.8.0
+require (
+	github.com/d5/tengo/v2 v2.10.0
+	github.com/gorilla/mux v1.8.0
+	github.com/stretchr/testify v1.7.0
+	github.com/tysonmote/gommap v0.0.1
+	google.golang.org/protobuf v1.27.1
+)
 
 require (
-	github.com/golang/protobuf v1.5.2 // indirect
-	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
-	google.golang.org/protobuf v1.27.1 // indirect
+	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
---
+++ internal/log/index.go
@@ -0,0 +1,57 @@
+package log
+
+import (
+	"os"
+
+	"github.com/tysonmote/gommap"
+)
+
+// define the number of bytes that make up each index entry
+var (
+	offWidth uint64 = 4
+	posWidth uint64 = 8
+	entWidth        = offWidth + posWidth
+)
+
+type index struct {
+	file *os.File    // persisted file
+	nmap gommap.MMap // persisted file
+	size uint64      // size of the index and where to write the next entry appended to the index
+}
+
+func newIndex(f *os.File, c Config) (*index, error) {
+	idx := &index{
+		file: f,
+	}
+	fi, err := os.Stat(f.Name())
+	if err != nil {
+		return nil, err
+	}
+	idx.size = uint64(fi.Size())
+	if err = os.Truncate(
+		f.Name(), int64(c.Segment.MaxIndexBytes),
+	); err != nil {
+		return nil, err
+	}
+	if idx.nmap, err = gommap.Map(
+		idx.file.Fd(),
+		gommap.PROT_READ|gommap.PROT_WRITE,
+		gommap.MAP_SHARED,
+	); err != nil {
+		return nil, err
+	}
+	return idx, nil
+}
+
+func (i *index) Close() error {
+	if err := i.mmap.Sync(gommap.MS_SYNC); err != nil {
+		return err
+	}
+	if err := i.file.Sync(); err != nil {
+		return err
+	}
+	if err := i.file.Truncate(int64(i.size)); err != nil {
+		return err
+	}
+	return i.file.Close()
+}
